c9f56bd73c9457038cda461ebad3115eb7f6e5d0
christian.abraham <christian.abraham@email.cz>
2022-02-16 10:38:19 +0100

    negr

--- src/main/java/com/company/SendEmail.java
+++ src/main/java/com/company/SendEmail.java
@@ -9,52 +9,47 @@ import java.util.Date;
 import java.util.Properties;
 
 
-public class SendEmail extends Object{
+public class SendEmail{
 
     public static void main(String [] args)
     {
 
-        try{
+        try {
+
+            String host = "smtp.gmail.com";
 
             Properties props = new Properties();
-            props.put("mail.smtp.host", "smtp.mail.yahoo.com");
-            props.put("mail.smtp.host", "smtp.mail.seznam.cz");
-            props.put("mail.smtp.host", "smtp.mail.email.cz");
-            props.put("mail.smtp.host", "smtp.mail.gmail.com");// for gmail use smtp.gmail.com
-            props.put("mail.smtp.auth", "true");
-            props.put("mail.debug", "true");
-            props.put("mail.smtp.starttls.enable", "true");
-            props.put("mail.smtp.port", "465");
-            props.put("mail.smtp.socketFactory.port", "465");
             props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-            props.put("mail.smtp.socketFactory.fallback", "false");
-
+            props.put("mail.smtp.auth", true);
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+            props.put("mail.smtp.port", 587);
+            props.put("mail.smtp.ssl.trust", host);
             Session mailSession = Session.getInstance(props, new javax.mail.Authenticator() {
 
                 protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication("CurrencyWatcher@email.cz", "KartingWithMazlik");
+                    return new PasswordAuthentication("currencywatcher2022@gmail.com", "KartingWithMazlik");
                 }
             });
 
-            mailSession.setDebug(true); // Enable the debug mode
+                mailSession.setDebug(true);
 
-            Message msg = new MimeMessage( mailSession );
+                Message msg = new MimeMessage(mailSession);
+                msg.setFrom(new InternetAddress("KartingWithMazlik"));
+                msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse("vit.vo@email.cz"));
+                msg.setSentDate(new Date());
+                msg.setSubject("Negr");
 
-            //--[ Set the FROM, TO, DATE and SUBJECT fields
-            msg.setFrom( new InternetAddress( "KartingWithMazlik" ) );
-            msg.setRecipients( Message.RecipientType.TO,InternetAddress.parse("filipusnovakus@seznam.cz") );
-            msg.setSentDate( new Date());
-            msg.setSubject( "Hello World!" );
+                msg.setText("Jsi pořádnej negr jak Smileskej komín");
+                msg.getMessageNumber();
 
-            //--[ Create the body of the mail
-            msg.setText( "Hello from my first e-mail sent with JavaMail" );
+                Transport.send(msg);
 
-            //--[ Ask the Transport class to send our mail message
-            Transport.send( msg );
+            }catch(Exception E){
+                System.out.println("Seš negr");
+                System.out.println(E);
+            }
 
-        }catch(Exception E){
-            System.out.println( "Oops something has gone pearshaped!");
-            System.out.println( E );
-        }
     }
 }
