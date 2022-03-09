7d474c040d71b022f9b86365cf1efed2787c25dd
jan.polacek <gg.polacek@gmail.com>
2022-03-09 10:23:38 +0100

    Hovno

--- application/src/main/java/cz/spsmb/service/CSOBDataConvertor.java
+++ application/src/main/java/cz/spsmb/service/CSOBDataConvertor.java
@@ -1,18 +1,21 @@
 package main.java.cz.spsmb.service;
 
+import main.java.cz.spsmb.entity.BankCode;
 import main.java.cz.spsmb.entity.CurrencyEntity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CSOBDataConvertor implements DataConvertor {
     @Override
     public List<CurrencyEntity> convert(String content) {
-        List<CurrencyEntity> currencyEntities;
-        Object[] contentArray = content.lines().toArray();
+        List<CurrencyEntity> currencyEntities = new ArrayList<>();
+        String[] contentArray = content.split("\n");
         for (int i = 4; i < contentArray.length; i++) {
-            System.out.println(contentArray[i]);
-            currencyEntities.add(new CurrencyEntity("CSOB", contentArray[i]., "", ""));
+            String[] dataArray = contentArray[i].toString().split(";");
+            System.out.println(dataArray[2] + "; " + dataArray[1] + "; " + Double.parseDouble(dataArray[6].replace(',', '.')));
+            currencyEntities.add(new CurrencyEntity(BankCode.CSOB, dataArray[2], Integer.parseInt(dataArray[1]), Double.parseDouble(dataArray[6].replace(',', '.'))));
         }
-        return null;
+        return currencyEntities;
     }
 }
